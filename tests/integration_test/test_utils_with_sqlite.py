1c97666392d8e9b1a0249e546cb05c72415c9298
benhid <antoniobenitezhid@gmail.com>
2025-03-28 19:26:31 +0100

    Add documentation and start implementation on tests

---
+++ tests/integration_test/test_utils_with_sqlite.py
@@ -0,0 +1,11 @@
+
+
+def setup():
+    # Prepare the environment
+    pass
+
+def tear_down():
+    #Clean up after tests
+    pass
+
+def test_check_if_project_exists():
---
+++ tests/unit_test/test_models.py
@@ -0,0 +1,9 @@
+import pytest
+
+from crud_api.models import Project
+from pydantic import ValidationError
+
+def test_time_cant_be_negative():
+    with pytest.raises(ValidationError):
+        Project(name="test", time=-1)
+
